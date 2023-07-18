6b56c23770deaa34a4cae5017f24f804087e2d65
alathea23 <135459921+alathea23@users.noreply.github.com>
2023-07-17 18:22:07 -0700

    adding in functionality and styling header area

--- script.js
+++ script.js
@@ -1,31 +1,141 @@
-var startBtn = document.querySelector("#start-btn")
+//establishes base variables
+var startBtn = document.getElementByID('start-btn')
+var nextBtn = document.getElementByID("next-btn")
+var restartBtn = document.getElementByID("restart")
+var highScore = document.getElementByID("user-score")
+var timer = document.getElementByID("timer")
+var QuestionText = document.getElementByID("question");
+var answerText = document.getElementByID("answers");
+var answer1text = document.getElementByID("A1")
+var answer2text = document.getElementByID("A2")
+var answer3text = document.getElementByID("A3")
+var answer4text = document.getElementByID("A4")
+const quizContainerElement = document.getElementById('quiz-container')
+const questionContainerElement = document.getElementById('question-container')
+
+var questionNum = 0
+var testScore = 0
+
+var shuffledQuestions, questionNum
 
 
-var QuestionText = document.querySelector("#question");
-var answer1text = document.querySelector("#A1")
-var answer2text = document.querySelector("#A2")
-var answer3text = document.querySelector("#A3")
-var answer4text = document.querySelector("#A4")
+// holds the information for the quiz questions and available answers
+var quizQuestions = [
+    {
+        question: "Commonly Used Data Types do NOT include: ?",
+        answers: [
+           { text: "boolean", correct: false},
+           { text: "alerts", correct: true},
+            { text: "numbers", correct: false},
+            { text: "strings", correct: false}]
+        },
+    {
+        question: "The condition in an if/else statement is enclosed with _____ ?",
+        answers: [
+            { text: "curly brackets", correct: true},
+            { text: "parentheses", correct: false},
+            { text: "straight brackets", correct: false},
+            { text: "quotation marks", correct: false},
+        ],
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____??",
+        answers: [
+            { text: "boolean values", correct: false},
+            { text: "numbers", correct: false},
+            { text: "strings", correct: false},
+            { text: "all of the above", correct: true},
+ ] },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables?",
+        answers: [
+            { text:  "curly brackets",  correct: false},
+            { text:  "parentheses", correct: false},
+            { text:  "straight brackets", correct: false},
+            { text:  "quotation marks", correct: false},
+  ]  
+    }
+]
 
-var questList = ["Commonly Used Data Types do NOT include: ?", "The condition in an if/else statement is enclosed with _____ ?", "Arrays in JavaScript can be used to store ____?", "String values must be enclosed within _____ when being assigned to variables?" ]
-var A1List = ["boolean", "curly brackets", "boolean values", "curly brackets"]
-var A2List = ["alerts", "parentheses", "numbers", "parentheses"]
-var A3List = ["numbers", "straight brackets", "strings", "straight brackets"]
-var A4List = ["strings", "quotation marks", "All of the above", "quotation marks"]
+function selectAnswer() {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerText.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > questionNum + 1) {
+      nextBtn.classList.remove('hide')
+    } else {
+      startBtn.innerText = 'Restart'
+      startBtn.classList.remove('hide')
+    }
+  }
+
+//generates quiz questions
+  
+  function showQuestion(question) {
+    QuestionText.innerText = question.question
+    question.answers.forEach(answer => {
+      const button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+      if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerText.appendChild(button)
+    })
+  }
+
+  function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[questionNum])
+  }
 
 function quizGen() {
-var question = questionGen();
-var answer1 = A1gen();
-var answer2 = A2gen();
-var answer3 = A3gen();
-var answer4 = A4gen();
+    startBtn.classList.add("hide")
+    QuestionText.classList.remove("hide")
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    questionNum = 0
+    setNextQuestion()
+    showQuestion()
+}
 
-QuestionText.value = question;
-answer1text.value = answer1
-answer2text.value = answer2
-answer3text.value = answer3
-answer4text.value = answer4
-};
+//goes to the next question in the list
+
+    QuestionText.value = question;
+    answer1text.value = answer1
+    answer2text.value = answer2
+    answer3text.value = answer3
+    answer4text.value = answer4
 
 
-startBtn.addEventListener("click", quizGen)
+  function resetState() {
+    clearStatusClass(document.body)
+    nextBtn.classList.add('hide')
+    while (answerText.firstChild) {
+      answerText.removeChild(answerText.firstChild)
+    }
+  }
+  
+  function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
+
+  startBtn.addEventListener("click", quizGen())
+  nextBtn.addEventListener("click", () => {
+      questionNum++, setNextQuestions ()
+  })
+  restartBtn.addEventListener("click", quizGen())
+  
